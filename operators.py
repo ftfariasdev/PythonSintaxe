ce2a6800a770df3e80042881552b69a60020e6fa
Felipe Teixeira Farias <farias_felipe@outlook.com.br>
2026-02-19 22:05:17 -0300

    feat: Add operators demonstration in Python (#4)

---
+++ operators.py
@@ -0,0 +1,58 @@
+# Operators in Python
+
+print("=" * 60)
+print("Comparison Operators in Python")
+# comparison operators
+a = 10
+b = 5
+
+print(a > b) # Output: True
+print(a < b)  # Output: False
+print(a == b) # Output: False
+print(a != b) # Output: True
+
+# arithmetic operators
+
+print("=" * 60)
+print("Arithmetic Operators in Python")
+
+# imports types
+import math
+from math import sqrt
+from math import *
+import math as mt
+
+addition = 1 + 1
+print(addition) # Output: 2
+
+subtraction = 1 - 1
+print(subtraction) # Output: 0  
+
+multiplication = 2 * 1
+print(multiplication) # Output: 2
+
+division = 5 / 2
+print(division) # Output: 2.5
+
+divisionFloor = 5 // 2
+print(divisionFloor) # Output: 2
+
+exponentiation = 2 ** 2
+print(exponentiation) # Output: 4
+
+squareRoot = math.sqrt(100)
+print(squareRoot) # Output: 10.0
+
+print("=" * 60)
+
+# Logical Operators in Python
+print("Logical Operators in Python")
+
+x = True
+y = False
+print(x and y) # Output: False
+print(x or y)  # Output: True
+print(not x)   # Output: False
+print(not y)   # Output: True
+
+print("=" * 60)
