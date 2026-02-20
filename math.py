b2f9b3be18f9d5e300fbcc3704d9aca1e8368315
Felipe Teixeira Farias <farias_felipe@outlook.com.br>
2026-02-19 22:04:41 -0300

    feat:Adding different types math operations (#5)

---
+++ math.py
@@ -0,0 +1,13 @@
+# Types to import
+import math
+from math import sqrt
+from math import *
+import math as mt
+
+addition = 1 + 1
+subtraction = 1 - 1
+multiplication = 2 * 1
+division = 5 / 2
+divisionFloor = 5 // 2
+exponentiation = 2 ** 2
+squareRoot = math.sqrt(100)
